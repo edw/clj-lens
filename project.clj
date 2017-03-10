684bb61acc7cfe4b04e29e7be64cbce733c81433
Edwin Watkeys <edw@poseur.com>
2017-03-10 11:33:04 -0500

    Fix license name (EPL->MIT)

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject org.clojars.edw/clj-lens "0.1.3"
+(defproject org.clojars.edw/clj-lens "0.1.4"
   :description "Nested data structure querying and updating"
   :url "http://github.com/edw/clj-lens"
-  :license {:name "Eclipse Public License"
+  :license {:name "MIT License"
             :url "https://github.com/edw/clj-lens/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
